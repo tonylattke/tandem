0193e11e99cc46d6acc9992f1d4cfbfdaa6a1826
Tony Lattke <tonylattke@gmail.com>
2014-12-12 22:42:52 +0100

    welcome site

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :welcome
+
   resources :mail_messages
 
   resources :friendships
@@ -16,7 +18,10 @@ Rails.application.routes.draw do
   resources :messages
 
   devise_for :users
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
+  root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
