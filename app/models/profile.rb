d9ab7f630eaf4b00f85eca35636cbb2bf76668e5
Tony Lattke <tonylattke@gmail.com>
2014-12-12 22:02:56 +0100

    photo profile atribute

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,11 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
   belongs_to :language
+  has_attached_file :photo, :styles => { :small => "168x168>" },
+  :url  => "/assets/profile/:id/:style/:basename.:extension",
+  :path => ":rails_root/public/assets/profiles/:id/:style/:basename.:extension"
+
+  validates_attachment_presence :photo
+  validates_attachment_size :photo, :less_than => 5.megabytes
+  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png', 'image/jpg', 'image/gif']
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141212203854) do
+ActiveRecord::Schema.define(version: 20141212204804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,10 @@ ActiveRecord::Schema.define(version: 20141212203854) do
     t.string   "answer"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   add_index "profiles", ["language_id"], name: "index_profiles_on_language_id", using: :btree
