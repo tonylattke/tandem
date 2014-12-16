67d3bc9f17e011b3d5ea63cc5383a62b62ec8b3b
Tony Lattke <tonylattke@gmail.com>
2014-12-16 22:25:17 +0100

    profile - photo url fixed

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,8 +3,8 @@ class Profile < ActiveRecord::Base
   belongs_to :language
 
   has_attached_file :photo, :styles => { :small => "168x168>" },
-  :url  => "/assets/profile/:id/:style/:basename.:extension",
-  :path => ":rails_root/public/assets/profiles/:id/:style/:basename.:extension"
+    :url  => "/assets/profiles/:id/:style/:basename.:extension",
+    :path => ":rails_root/public/assets/profiles/:id/:style/:basename.:extension"
 
   validates_attachment_presence :photo
   validates_attachment_size :photo, :less_than => 5.megabytes
