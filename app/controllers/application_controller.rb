ced175c4767a2d919c748504f476dc0f9319e970
Tony Lattke <tonylattke@gmail.com>
2015-02-08 21:02:59 +0100

    New login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :get_user
+
+  def get_user
+    if user_signed_in?
+      @user_profile = Profile.find_by(user: current_user.id)
+    end
+  end
+
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
 	def index
-
+		@info = "asd"
+		if user_signed_in?
+			redirect_to "/profiles"
+		end
 	end
 end
