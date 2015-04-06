054acbf6f05533e825445e0168fe3d145963bb72
Tony Lattke <tonylattke@gmail.com>
2015-04-06 23:38:44 +0200

    CSS start page and messages in groups

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,11 +7,11 @@ class WelcomeController < ApplicationController
 	end
 
 	def inbox
-		@mail_messages = MailMessage.where("recipient_id = ?",current_user)
+		@mail_messages = MailMessage.where("recipient_id = ?",current_user).group(:sender_id, :id)
 	end
 
 	def sent
-		@mail_messages = MailMessage.where("sender_id = ?",current_user)
+		@mail_messages = MailMessage.where("sender_id = ?",current_user).group(:recipient_id, :id)
 	end
 
 end
